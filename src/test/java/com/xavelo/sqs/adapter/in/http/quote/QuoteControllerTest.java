db2047387b6c2dd3e9606cb8ad883c33cfff5d73
xavelo <xavelo@gmail.com>
2025-06-11 19:09:18 +0200

    Add controller tests

---
+++ src/test/java/com/xavelo/sqs/adapter/in/http/quote/QuoteControllerTest.java
@@ -0,0 +1,131 @@
+package com.xavelo.sqs.adapter.in.http.quote;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.xavelo.sqs.application.domain.ArtistQuoteCount;
+import com.xavelo.sqs.application.domain.Quote;
+import com.xavelo.sqs.port.in.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(QuoteController.class)
+class QuoteControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean private StoreQuoteUseCase storeQuoteUseCase;
+    @MockBean private GetQuotesUseCase getQuotesUseCase;
+    @MockBean private GetQuoteUseCase getQuoteUseCase;
+    @MockBean private GetRandomQuoteUseCase getRandomQuoteUseCase;
+    @MockBean private CountQuotesUseCase countQuotesUseCase;
+    @MockBean private DeleteQuoteUseCase deleteQuoteUseCase;
+    @MockBean private GetArtistQuoteCountsUseCase getArtistQuoteCountsUseCase;
+
+    @Test
+    void createQuote() throws Exception {
+        Quote quote = new Quote(null, "line", "song", "album", 1990, "artist", 0, 0);
+        when(storeQuoteUseCase.storeQuote(quote)).thenReturn(1L);
+
+        mockMvc.perform(post("/api/quote")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(quote)))
+                .andExpect(status().isOk())
+                .andExpect(content().string("1"));
+    }
+
+    @Test
+    void createQuotes() throws Exception {
+        List<Quote> quotes = List.of(new Quote(null, "a", "s", "al", 2000, "art", 0, 0));
+        when(storeQuoteUseCase.storeQuotes(quotes)).thenReturn(List.of(1L));
+
+        mockMvc.perform(post("/api/quotes")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(quotes)))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[1]"));
+    }
+
+    @Test
+    void getQuotes() throws Exception {
+        List<Quote> quotes = List.of(new Quote(1L, "q", "s", "a", 1999, "art", 0, 0));
+        when(getQuotesUseCase.getQuotes()).thenReturn(quotes);
+
+        mockMvc.perform(get("/api/quotes"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(quotes)));
+    }
+
+    @Test
+    void getArtists() throws Exception {
+        List<ArtistQuoteCount> artists = List.of(new ArtistQuoteCount("art", 2L));
+        when(getArtistQuoteCountsUseCase.getArtistQuoteCounts()).thenReturn(artists);
+
+        mockMvc.perform(get("/api/artists"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(artists)));
+    }
+
+    @Test
+    void getQuotesCount() throws Exception {
+        when(countQuotesUseCase.countQuotes()).thenReturn(5L);
+
+        mockMvc.perform(get("/api/quotes/count"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("5"));
+    }
+
+    @Test
+    void getRandomQuoteFound() throws Exception {
+        Quote quote = new Quote(1L, "q", "s", "a", 1999, "art", 0, 0);
+        when(getRandomQuoteUseCase.getRandomQuote()).thenReturn(quote);
+
+        mockMvc.perform(get("/api/quote/random"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(quote)));
+    }
+
+    @Test
+    void getRandomQuoteNotFound() throws Exception {
+        when(getRandomQuoteUseCase.getRandomQuote()).thenReturn(null);
+
+        mockMvc.perform(get("/api/quote/random"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void getQuoteFound() throws Exception {
+        Quote quote = new Quote(1L, "q", "s", "a", 1999, "art", 0, 0);
+        when(getQuoteUseCase.getQuote(1L)).thenReturn(quote);
+
+        mockMvc.perform(get("/api/quote/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(quote)));
+    }
+
+    @Test
+    void getQuoteNotFound() throws Exception {
+        when(getQuoteUseCase.getQuote(2L)).thenReturn(null);
+
+        mockMvc.perform(get("/api/quote/2"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void deleteQuote() throws Exception {
+        mockMvc.perform(delete("/api/quote/1"))
+                .andExpect(status().isNoContent());
+    }
+}
