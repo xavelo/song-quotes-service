487fb12be19aa09875062996a6c5a52d1f4cd2e5
xavelo <xavelo@gmail.com>
2025-06-12 20:48:17 +0200

    Add Flyway support

---
+++ src/main/resources/db/migration/V1__create_quotes_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE quotes (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    quote VARCHAR(1000),
+    song VARCHAR(255),
+    album VARCHAR(255),
+    year INT,
+    artist VARCHAR(255),
+    posts INT NOT NULL DEFAULT 0,
+    hits INT NOT NULL DEFAULT 0
+);
