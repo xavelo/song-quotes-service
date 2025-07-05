98bfc06040a568ec8f5bf92504bf1e7f1f752817
xavelo <xavelo@gmail.com>
2025-07-06 00:33:58 +0200

    feat: Configure Spotify API credentials and token retrieval

--- src/main/java/com/xavelo/sqs/adapter/out/spotify/client/SpotifySearchClient.java
+++ src/main/java/com/xavelo/sqs/adapter/out/spotify/client/SpotifySearchClient.java
@@ -3,8 +3,9 @@ package com.xavelo.sqs.adapter.out.spotify.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import com.xavelo.sqs.configuration.spotify.SpotifyFeignClientConfiguration;
 
-@FeignClient(name = "spotify-search", url = "https://api.spotify.com/v1")
+@FeignClient(name = "spotify-search", configuration = SpotifyFeignClientConfiguration.class)
 public interface SpotifySearchClient {
 
     @GetMapping("/search?type=artist")
