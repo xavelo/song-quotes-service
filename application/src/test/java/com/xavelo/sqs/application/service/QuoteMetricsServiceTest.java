62d21df3d4dbe50423510f5c4442a01db112ac55
xavelo <xavelo@gmail.com>
2025-09-28 18:15:45 +0200

    Decouple metrics updates from quote service

---
+++ application/src/test/java/com/xavelo/sqs/application/service/QuoteMetricsServiceTest.java
@@ -0,0 +1,48 @@
+package com.xavelo.sqs.application.service;
+
+import com.xavelo.sqs.application.domain.Quote;
+import com.xavelo.sqs.application.service.event.QuoteHitEvent;
+import com.xavelo.sqs.application.service.event.QuoteStoredEvent;
+import com.xavelo.sqs.port.out.MetricsPort;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+@ExtendWith(MockitoExtension.class)
+class QuoteMetricsServiceTest {
+
+    @Mock
+    private MetricsPort metricsPort;
+
+    @InjectMocks
+    private QuoteMetricsService quoteMetricsService;
+
+    private Quote quote;
+
+    @BeforeEach
+    void setUp() {
+        quote = new Quote(1L, "quote", "song", "album", 1990, "artist", 0, 1, null);
+    }
+
+    @Test
+    void onQuoteHit_incrementsHitMetrics() {
+        quoteMetricsService.onQuoteHit(new QuoteHitEvent(quote));
+
+        verify(metricsPort).incrementTotalHits();
+        verify(metricsPort).incrementQuoteHits(quote.id());
+    }
+
+    @Test
+    void onQuoteStored_onlyIncrementsStoredMetric() {
+        quoteMetricsService.onQuoteStored(new QuoteStoredEvent(quote));
+
+        verify(metricsPort).incrementStoredQuotes();
+        verifyNoMoreInteractions(metricsPort);
+    }
+}
