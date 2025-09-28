0d04a1ccfe71560dfc7ecfa5aff535464e5fec89
xavelo <xavelo@gmail.com>
2025-09-28 09:25:18 +0200

    Fix test execution by generating API archive earlier

--- application/src/main/java/com/xavelo/sqs/adapter/in/http/quote/mapper/QuoteMapper.java
+++ application/src/main/java/com/xavelo/sqs/adapter/in/http/quote/mapper/QuoteMapper.java
@@ -6,7 +6,7 @@ import org.mapstruct.Mapper;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", implementationName = "QuoteHttpMapperImpl")
 public interface QuoteMapper {
 
     QuoteDto toDto(Quote quote);
